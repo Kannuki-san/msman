6eae57428d5d33325682079b6ac39d6d631a3979
Kannuki_san <Kannu@DESKTOP-MMAOT8M>
2021-01-13 23:41:49 +0900

    master

--- test.py
+++ test.py
@@ -1,3 +1,20 @@
 import os
+import sys
+import subprocess as spr
+from subprocess import PIPE
+import tkinter as tk
+from tkinter.constants import CENTER
 
-os.system('java -jar mohist-1.12.2-165-server.jar')
+#window
+window = tk.Tk()
+window.title("serverconsole")
+
+window.geometry('400x300')
+
+
+proc = spr.run("java -jar mohist-1.12.2-165-server.jar", shell=True, stdout=PIPE, stderr=PIPE, text=True)
+output = tk.Label(text=proc, width=50)
+output.pack(anchor='center')
+
+
+window.mainloop()
